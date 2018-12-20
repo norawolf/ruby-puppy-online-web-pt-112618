cd908f2fb748d3669052b9fc1cb9b0dc679e48bf
Nora Vogt <noraevogt@gmail.com>
2018-12-20 03:42:07 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    puts @@all.map {|x| x.name}
+  end
+end
